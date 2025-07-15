51faa1a8e783997423ac4f041358af89ecfb113b
SAdebug <sarthakmasterayachi@gmail.com>
2025-07-15 13:49:28 +0000

    data types

---
+++ 01_basics/data_types.js
@@ -0,0 +1,2 @@
+"use strict";//helps you write safer and cleaner code by catching common mistakes and preventing the use of certain bad practices,basically it makes JS stricter.
+//alert() --> it shows a pop-up message to the user,it contains only plain texts,without any style.The webpage becomes blocked until the user clicks OK.We are using Nodejs,not browser console.
