72e485f0f721c91d2dc0ddfb93761253a8fed5fb
SAdebug <sarthakmasterayachi@gmail.com>
2025-07-15 18:09:40 +0000

    data types

---
+++ 01_basics/01_data_types.js
@@ -0,0 +1,12 @@
+"use strict";//helps you write safer and cleaner code by catching common mistakes and preventing the use of certain bad practices,basically it makes JS stricter.
+//alert() --> it shows a pop-up message to the user,it contains only plain texts,without any style.The webpage becomes blocked until the user clicks OK.We are using Nodejs,not browser console.
+// 7 primitive data types-> string,number,bigint,boolean,symbol(for uniqueness),null(it is an object),undefined.
+// There is one more type which is called an object(see the handwritten notes).
+let mobileNumber = 123
+let name = "Rahul"
+let address//this is undefined
+// typeof tells you the data type:
+console.table([typeof name,typeof address,typeof mobileNumber,typeof null]);
+// => null is not a type,it is an object.
+console.table([typeof sdjfbjehfbqw,typeof string,typeof undefined])
+// if any random word is written in front of typeof then it will o/p 'undefined'.Also,(typeof undefined) will also o/p undefined.
