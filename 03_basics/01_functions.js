cf75657e49182b6ff89b5cddc94a4f25d19cf01c
SAdebug <sarthakmasterayachi@gmail.com>
2025-07-25 22:43:16 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -2,3 +2,10 @@
 function printValues(...num){
     return num
 }
+//console.log(printValues(100,200,300,400));
+//Jahan se rest operator shuru hota hai,vahan se multiple arguments allowed hai aur vo arguments ek array ke andar aa jate hai.
+function printValues2(num1,num2,...num){
+    return num
+}
+console.log(printValues2(1,2,3,4,5,6,7));//[3,4,5,6,7]
+console.log(typeof printValues2(1,2,3,4,5,6,7));//object
