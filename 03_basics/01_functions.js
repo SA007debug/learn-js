63a6c189b7cc72ea6a10db13cce845f22c61ad6f
SAdebug <sarthakmasterayachi@gmail.com>
2025-07-25 22:18:20 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,4 @@
+//If you don't know the exact amount of arguments in the function and you want to add the arguments dynamically(like in a shopping cart),then you can use rest operator:
+function printValues(...num){
+    return num
+}
