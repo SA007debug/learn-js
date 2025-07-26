f8ad4d480a9b46b8fcd54d3648de457618b45468
SAdebug <sarthakmasterayachi@gmail.com>
2025-07-26 22:55:14 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -7,5 +7,43 @@ function printValues(...num){
 function printValues2(num1,num2,...num){
     return num
 }
-console.log(printValues2(1,2,3,4,5,6,7));//[3,4,5,6,7]
-console.log(typeof printValues2(1,2,3,4,5,6,7));//object
+//console.log(printValues2(1,2,3,4,5,6,7));//[3,4,5,6,7]
+//console.log(typeof printValues2(1,2,3,4,5,6,7));//object
+
+
+//Scope:
+//var scope ke bahar chala jata hai!!
+if(true){
+    var a = 20
+    const b = 25
+}
+a = 100
+// console.log(a);//100
+// console.log(b);//error:b is not defined.
+let a1 = 10
+if(true){
+    let a1 = 200//ye andar hi reh jayega.
+    const b1 = 20
+}
+//console.log(a1);//10
+//Agar koi variable declaration scope ke andar aata hai to vo bahar sirf tabhi jayega agar (i)var use hua ho ya phir (ii)bina kisi keyword kay declare kiya ho;agar let ya const use hua hai to vo scope ke andar hi reh jayega.
+let a2 = 10
+if(true){
+    a2 = 200//ye bahar aa jayega
+    const b2 = 20
+}
+//console.log(a2);//200
+//Note: Global scope is different in console of webpage and in node(vs code).
+
+
+function one(){
+    const username = "Sarthak"
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+    }
+    two()
+    console.log(website);
+}
+one()
+//Kisi bhi function ke andar tabhi jana hai jab use call kiya ho.
