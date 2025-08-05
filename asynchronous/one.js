db6b0d2601db2b0ffb12cea69765fb8d1f9bde95
SAdebug <sarthakmasterayachi@gmail.com>
2025-08-05 20:16:07 +0000

    asynchronous coding

--- asynchronous/one.js
+++ asynchronous/one.js
@@ -3,10 +3,10 @@
 //Asynchronous: The task which takes time is executed parallely and the code moves on to the next task.
 //setTimeout(func_name,time):this executes the function after some time.That time is given by the user(in milliseconds).
 //setTimeout expects a function as its first argument — not the result of a function call!!
-function greet(){
-    console.log(`How are you?`);
-}
-setTimeout(greet,7000)//this will be executed after 7 seconds than usual.
+// function greet(){
+//     console.log(`How are you?`);
+// }
+// setTimeout(greet,7000)//this will be executed after 7 seconds than usual.
 //NOTE: Agar setTimeout ke andar greet ke baad bracket lagaya to function turant call ho jayega aur error dega.
 // function greet(){
 //     console.log(`How are you?`);
@@ -25,4 +25,13 @@ setTimeout(greet,7000)//this will be executed after 7 seconds than usual.
 
 
 //Callback:
-//
+//A function which is passed as an argument to another function is called a callback.
+// function greet(name){
+//     console.log(`How are you ${name}?`)
+// }
+// function greet2(name,func){
+//     func(name);
+// }
+// greet2(greet,"Sam")
+//Here,greet is a callback.
+//setTimeout also uses callback.
